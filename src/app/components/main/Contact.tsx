009d34641e4ce4e7c8ea137d37677fb4099cee64
Huzaifa-Abdurahman <141252841+Huzaifa-Abdurahman@users.noreply.github.com>
2024-07-29 06:19:01 -0700

    Update Contact.tsx

--- src/app/components/main/Contact.tsx
+++ src/app/components/main/Contact.tsx
@@ -9,7 +9,7 @@ function Contact() {
       
       <h1 className='font-bold text-3xl my-8 text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 '>Contact us</h1>
       <div className='flex flex-col md:flex-row gap-10 px-10 '>
-        <div className=' overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] m-2 p-2 py-6 px-6 flex flex-col'>
+        <div className='p-2 overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] m-2 p-2 py-6 px-6 flex flex-col'>
           <div className='font-sans text-2xl'>
           <div className="flex flex-col">
           <div className=''>
@@ -22,10 +22,10 @@ function Contact() {
           <div className=''>Linkdin<RxLinkedinLogo /> huzaifa abdurahman</div></div><br />
           <div><div><Link href="https://wa.me/03100043155"><Image src="/whattsap.png" width={24} height={24} alt='whattsap' /></Link></div>+92-3100043155 Whatssap</div>
         </div></div>
-        <div className='relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] m-2 p-2 py-6 px-6  flex flex-col '>
-          <div className='font-sans text-2xl'><div> E-Mail <Image src="/gmail.png" width={24} height={24} alt='gmail' className='bg-white'/></div> 
-            abdurahhuzaifa393@gmail.com  <a href="mailto:abdurahhuzaifa393@gmail.com">Send email</a>
-          </div>
+        <div className='p-2 overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] m-2 p-2 py-6 px-6  flex flex-col '>
+          <div className='font-sans text-2xl'><div> E-Mail <Image src="/gmail.png" width={24} height={24} alt='gmail' className='bg-white'/></div> <br/>
+            galaxysoftwarehub@gmail.com  <a href="mailto:abdurahhuzaifa393@gmail.com">Send email</a>
+          </div> <br/>
           <div className='font-sans text-2xl'>Facebook <Image
           src='/f.png'
           width={24}
