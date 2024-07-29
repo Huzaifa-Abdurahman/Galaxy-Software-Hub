b8f637ba1af164eef17340c64feed5a5ea0dc378
Huzaifa-Abdurahman <141252841+Huzaifa-Abdurahman@users.noreply.github.com>
2024-07-29 06:22:02 -0700

    Update Navbar.tsx

--- src/app/components/main/Navbar.tsx
+++ src/app/components/main/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-auto flex flex-col items-center justify-between m-auto px-[10px]">
     
-        <div className="flex flex-row justify-between items-between px-20">
+        <div className="flex flex-row justify-between items-between px-5">
           <div><a
           href="#about-me"
           className="h-auto w-auto flex flex-row items-center"
