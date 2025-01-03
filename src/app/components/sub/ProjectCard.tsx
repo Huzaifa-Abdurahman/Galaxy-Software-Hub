7161ac450a0f0860c268b65a98b52322c65b7b41
Huzaifa-Abdurahman <141252841+Huzaifa-Abdurahman@users.noreply.github.com>
2025-01-03 11:38:33 +0500

    Update ProjectCard.tsx

--- src/app/components/sub/ProjectCard.tsx
+++ src/app/components/sub/ProjectCard.tsx
@@ -21,6 +21,14 @@ const ProjectCard = ({ src, title, description }: Props) => {
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
         <p className="mt-2 text-gray-300">{description}</p>
+            <Link href="https://huzaifa-abdurahman.github.io/Responsiv-Portfolio-in-html-and-css/">
+      <a>
+        <button className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition duration-200 ease-in-out">
+          Demo
+        </button>
+      </a>
+    </Link>
+
       </div>
     </div>
   );
